7444ae8042822b1204839dd003b31ce3f6943182
Addy <amgalvan@miners.utep.edu>
2021-02-11 11:13:06 -0700

    added more options for user to choose from

--- uimain.c
+++ uimain.c
@@ -7,7 +7,7 @@ int main()
 
   while (1) { // Infinite while loop
 
-    fputs("Select which shape you want to print (Triangle = t, Square = s, Arrow = a, Chars = c) or 'q' to quit\n> ", stdout);
+    fputs("Select which shape you want to print (Triangle = t, Square = s, Arrow = a, Vertical Chars = c, horizontal Chars = h) or 'q' to quit\n> ", stdout);
     fflush(stdout);		/* stdout only flushes automatically on \n */
     int c;
     while ((c = getchar()) == '\n'); /* ignore newlines */
@@ -29,7 +29,12 @@ int main()
       print_arrow(5, 7);
       break;
     case 'c':
-      puts("You selected chars:");
+      puts("You selected vertical chars:");
+      for (char c = 'A'; c < 'D'; c++)
+	print_char_8x12(c);
+      break;
+    case 'h':
+      puts("You selected horizontal chars:");
       for (char c = 'A'; c < 'D'; c++)
 	print_char_5x7(c);
       break;
